406662e4527c8474ba9c4c21d3f0c6f8db4e0426
PrashantFiles1 <109992225+PrashantFiles1@users.noreply.github.com>
2023-01-03 07:06:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Greymattersbot/Link-Search-Bot-Pro /MdiskWalaBot
-cd /MdiskWalaBot
+git clone https://github.com/PrashantFiles1/extrafilter /extrafilter
+cd /extrafilter
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
