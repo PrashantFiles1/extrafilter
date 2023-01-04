786022a4c821cf4ac36781e3db59c41a5148f001
PrashantFiles1 <109992225+PrashantFiles1@users.noreply.github.com>
2023-01-04 11:00:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ echo "Cloning Repo...."
 git clone https://github.com/PrashantFiles1/extrafilter /extrafilter
 cd /extrafilter
 pip3 install -r requirements.txt
-echo "Starting Bot...."
+echo "Starting extrafilter...."
 python3 main.py
