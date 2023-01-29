c9c6c48cc8ab21e7be726dfef75ef3aeee4aa9ff
PrashantFiles1 <109992225+PrashantFiles1@users.noreply.github.com>
2023-01-30 04:25:11 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -10,7 +10,7 @@ async def help_handler(_, event: Message):
     await event.reply_text(Config.ABOUT_HELP_TEXT.format(event.from_user.mention),
         reply_markup=InlineKeyboardMarkup([
             [
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{Config.BOT_USERNAME}?startgroup=true')
+            InlineKeyboardButton('ALL MOVIE CHANNELS', url=f'http://t.me/PRIME6BACKUP')
             ],
 
              [InlineKeyboardButton("About", callback_data="About_msg"),
@@ -35,7 +35,7 @@ async def start_handler(_,event: Message):
         caption=Config.START_MSG.format(event.from_user.mention),
         reply_markup=InlineKeyboardMarkup([
             [
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{Config.BOT_USERNAME}?startgroup=true')
+            InlineKeyboardButton('ALL MOVIE CHANNELS', url=f'http://t.me/http://t.me/PRIME6BACKUP')
             ],
 
              [InlineKeyboardButton("About", callback_data="About_msg"),
